299bc25068c0dcaf41b80774e569615064cbc5cd
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-10-05 15:19:08 -0500

    add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messageSenderId: '',
-  apiId: '',
+  apiKey: 'AIzaSyClogWwXEEa1eiNDYF4ORPVi9V_n3cnXKc',
+  authDomain: 'todoist-clone-d618b.firebaseapp.com',
+  databaseURL: 'https://todoist-clone-d618b.firebaseio.com',
+  projectId: 'todoist-clone-d618b',
+  storageBucket: 'todoist-clone-d618b.appspot.com',
+  messagingSenderId: '682143579270',
+  appId: '1:682143579270:web:e703efd61d5d9b36056507',
 });
 
 export { firebaseConfig as firebase };
